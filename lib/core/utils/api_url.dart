6821370c93ced268761a63dacf7b07a7933b941d
bens1001 <abdelhamid.bensalah.35@gmail.com>
2024-03-16 10:38:58 +0100

    app settings fix (android & ios)

--- lib/core/utils/api_url.dart
+++ lib/core/utils/api_url.dart
@@ -1,7 +1,9 @@
+import 'package:weatherapp_starter_project/core/utils/api_key.dart';
+
 String apiURL(double lat, double lon) {
   String url;
 
   url =
-      "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=1e238b5066217192b083caccfa46b2f0";
+      "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey";
   return url;
 }
